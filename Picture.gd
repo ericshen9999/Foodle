cca884f01033e4784bc8c72d7d0892d294180203
ericshen9999 <dragoneshen@gmail.com>
2022-05-02 22:47:08 -0700

    Cleaned up code

--- Picture.gd
+++ Picture.gd
@@ -5,24 +5,33 @@ extends Sprite
 # var a = 2
 # var b = "text"
 var zoomPhase = 1
-var baseScale
+var maxZoom = 6
+var initScale
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	baseScale = scale.x
-	region_rect = Rect2(texture.get_width()/2 * (0.167 * (6 - zoomPhase)), texture.get_width()/2 * (0.167 * (6 - zoomPhase)), texture.get_width() * (0.167 * zoomPhase), texture.get_height() * (0.167 * zoomPhase))
-	scale /= 0.167 * zoomPhase
+	#Middle zoom
+	calcZoom()
+	initScale = scale
+	scale /= ((1.0 / maxZoom) * zoomPhase)
 	pass # Replace with function body.
 
 
 func zoomOut():
 	print(zoomPhase)
 	zoomPhase += 1
-	region_rect = Rect2(texture.get_width()/2 * (0.167 * (6 - zoomPhase)), texture.get_width()/2 * (0.167 * (6 - zoomPhase)), texture.get_width() * (0.167 * zoomPhase), texture.get_height() * (0.167 * zoomPhase))
-	scale.x = 0.25
-	scale.y = 0.25
-	scale /= 0.167 * zoomPhase
-	if zoomPhase == 6:
+	calcZoom()
+	scale = initScale
+	scale /= ((1.0 / maxZoom) * zoomPhase)
+	if zoomPhase == maxZoom:
 		zoomPhase = 1
+
+func calcZoom():
+	region_rect = Rect2(
+	texture.get_width() / 2 * (0.167 * (6 - zoomPhase)), 
+	texture.get_width() / 2 * (0.167 * (6 - zoomPhase)), 
+	texture.get_width() * (0.167 * zoomPhase), 
+	texture.get_width() * (0.167 * zoomPhase))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
