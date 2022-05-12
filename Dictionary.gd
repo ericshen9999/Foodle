e35afacf56ffd4ff2d64b05093b302a51b6549e0
ericshen9999 <dragoneshen@gmail.com>
2022-05-12 05:06:49 -0700

    Added JSON and Correct Answer Check

--- Dictionary.gd
+++ Dictionary.gd
@@ -5,15 +5,36 @@ var maxCount
 var nodeImage
 var currentImage
 
+var file
+var path = "res://FoodData.json"
+var data
+var default_data = {
+	"1" : ["Noodle"],
+	"2" : ["Dumpling"]
+}
+
 func _ready() -> void:
 	nodeImage = get_node("/root/Main/Background/Food Image")
 	zoomCount = 1
 	currentImage = 1
 	maxCount = nodeImage.getMax()
+	
+	file = File.new()
+	if not file.file_exists(path):
+		data = default_data.duplicate(true)
+		print(data)
+		return
+	file.open(path, File.READ)
+	var text = file.get_as_text()
+	data = parse_json(text)
+	file.close()
+	print(data)
+	print(data["1"])
+	print(data["1"][0])
 
 func checkGuess(input):
-	if(false):
-		pass
+	if(data[str(currentImage)].has(input)):
+		print("Correct")
 	else:
 		zoomCount += 1
 		nodeImage.zoom(zoomCount)
@@ -21,8 +42,9 @@ func checkGuess(input):
 			failedImage()
 
 func failedImage():
+	print("Failure")
 	zoomCount = 1
 	currentImage += 1
+	if (currentImage > 2):
+		currentImage = 1
 	nodeImage.changeImage(currentImage)
-	if (currentImage >= 2):
-		currentImage = 0
